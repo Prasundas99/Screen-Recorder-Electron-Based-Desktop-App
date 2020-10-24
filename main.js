59aef79c2d70a3882eb3361b4e7770309351f116
Prasundas99 <prasundas115@gmail.com>
2020-10-24 12:21:50 +0530

    initiated code editor

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    frame:false,
+    frame: false,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
   });
 
   win.loadFile('codeeditor.html')
